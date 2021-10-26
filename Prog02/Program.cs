76bb73496483d948d55e7b3cf2564ad102412ee4
Irina <irina.filippova.mail@yandex.ru>
2021-10-26 10:02:32 +0300

    Lesson 01 edition

---
+++ Prog02/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+int x,y,max;
+Console.WriteLine("Укажите целое x");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Укажите целое y");
+y = Convert.ToInt32(Console.ReadLine());
+if (x>y)
+{
+   max=x;
+}
+else
+{
+   max=y;
+}
+Console.WriteLine("Max="+max);
